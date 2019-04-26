43030be82af62c99dc0162b3a74b10da18690f61
honestyman <leel50296@gmail.com>
2019-04-26 19:05:58 +0800

    update mobileHeader

--- src/components/MobileHeader/index.js
+++ src/components/MobileHeader/index.js
@@ -2,10 +2,15 @@ import React, { Component } from 'react'
 import { Sticky, Box, Divider, Text, Icon, Link} from 'gestalt'
 import './index.scss'
 export default class MobileHeader extends Component { 
+
+    shouldComponentUpdate(){ 
+        return false;
+    }
+
     render() { 
         return (
             <div className="mobile_header">
-                <div class="top_header">
+                <div className="top_header">
                     <Sticky top={0} dangerouslySetZIndex={{ __zIndex: 671 }}>
                         <Box color="white" shape="rounded" paddingX={8} paddingY={3}>
                             <Text bold align="center">StarImg</Text>
@@ -17,7 +22,7 @@ export default class MobileHeader extends Component {
                     <Divider />
                     <Sticky bottom={0} dangerouslySetZIndex={{ __zIndex: 671 }}>
                         <Box color="white" shape="rounded" paddingX={4} paddingY={2} display="flex" direction="row" alignItems="center">
-                            <Box marginRight={1} padding={1} flex="grow" alignContent="left">
+                            <Box marginRight={1} padding={1} flex="grow">
                                 <Link href="/">
                                    <Icon icon="pinterest" accessibilityLabel="Home" color="midnight" size="23" /> 
                                 </Link>
@@ -32,7 +37,7 @@ export default class MobileHeader extends Component {
                                     <Icon icon="heart" accessibilityLabel="Collect" color="midnight" size="23" />
                                 </Link>
                             </Box>
-                            <Box marginRight={1} padding={1} flex="grow" alignContent="right" className="test">
+                            <Box marginRight={1} padding={1} flex="grow">
                                 <Link href="/user">
                                     <Icon icon="person" accessibilityLabel="Person" color="midnight" size="23" />
                                 </Link>
