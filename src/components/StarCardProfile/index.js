d5e369e12f66373e7ab117b5b2b0e71bfd4898c5
honestyman <leel50296@gmail.com>
2019-04-27 16:29:50 +0800

    update style

--- src/components/StarCardProfile/index.js
+++ src/components/StarCardProfile/index.js
@@ -7,10 +7,12 @@ export default class SatrCardProfile extends Component {
     }
     render() { 
         return (
-            <Box display="flex" direction="row" paddingY={2} marginTop={1} color={'white'} >
+            <Box display="flex" direction="row" paddingX={1} paddingY={1} marginTop={1} color={'white'} alignItems="center">
                 <Box column={2}>
                     <Link href={this.props.domain} target={'blank'}>
-                        <Avatar name={this.props.name} src={this.props.avatar} verified={this.props.verified} />
+                        <Box>
+                            <Avatar name={this.props.name} src={this.props.avatar} verified={this.props.verified} />
+                        </Box>
                     </Link>
                 </Box>
                 <Box column={10} paddingX={2}>
