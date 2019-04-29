1f241286cb2328befa7907f7660151e7e2fe9a5b
honestyman <leel50296@gmail.com>
2019-04-29 16:58:07 +0800

    update store.js

--- src/store.js
+++ src/store.js
@@ -2,17 +2,14 @@ import { createStore, applyMiddleware,compose } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers/'
 
+
 const win = window
-const middlewares = [];
-if (process.env.NODE_ENV !== 'production') {
-  middlewares.push(thunk);
-}
+const middlewares = [thunk];
 
 const storeEnhancers = compose(
-  applyMiddleware(...middlewares),
-  (win && win.__REDUX_DEVTOOLS_EXTENSION__) ? win.__REDUX_DEVTOOLS_EXTENSION__() : (f) => f,
-);
-
+    applyMiddleware(...middlewares),
+    process.env.NODE_ENV !== 'production' && (win && win.__REDUX_DEVTOOLS_EXTENSION__) ? win.__REDUX_DEVTOOLS_EXTENSION__() : (f) => f,
+);  
 export default createStore(rootReducer, {}, storeEnhancers);
 // 打开浏览器拓展配置 Redux-DevTools
 // export default createStore(rootReducer, applyMiddleware(thunk));
