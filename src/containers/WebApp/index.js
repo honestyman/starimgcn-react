b5544a03e9f5b957bbde97ca8ac069f2071ab55d
honestyman <leel50296@gmail.com>
2019-05-19 15:43:25 +0800

    use React.lazy and Suspense

--- src/containers/WebApp/index.js
+++ src/containers/WebApp/index.js
@@ -1,35 +1,40 @@
-import React, { Component} from 'react'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import React, { Suspense, lazy, Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-import AsyncComponent from '../../components/AsyncComponent';
+import Header from "../../layouts/Header";
+import StarToast from "../../components/Toast";
+import Collect from "../Collect";
 
-import Header from '../../layouts/Header'
-import StarToast from '../../components/Toast'
-import Collect from '../Collect'
+// 动态引入组件
+// React.lazy 和 Suspense 技术还不支持服务端渲染
+const AsyncPins = lazy(() => import("../Pins"));
+const AsyncStarList = lazy(() => import("../StarList"));
+const AsyncStar = lazy(() => import("../Star"));
 
-const AsyncPins= AsyncComponent(() => import('../Pins'))
-const AsyncStarList = AsyncComponent(() => import('../StarList'))
-const AsyncStar = AsyncComponent(() => import('../Star'))
-
-export default class WebApp extends Component { 
-    
-    render() { 
+export default class WebApp extends Component {
+    render() {
         return (
             <Router>
                 <div className="webapp">
                     <Header />
-                    <Switch>
-                        <Route exact path="/" component={AsyncPins} />
-                        <Route exact path="/explore" component={AsyncStarList} />
-                        <Route exact path="/collect" component={Collect} />
-                        <Route exact path="/user" component={Collect} />
-                        <Route path="/:domain" component={AsyncStar} />
-                    </Switch>
+                    {/* 使用加载指示器为此组件做优雅降级 */}
+                    <Suspense fallback={<div style={{textAlign: "center",padding: "20px 15px"}}>Loading...</div>}>
+                        <Switch>
+                            <Route exact path="/" component={AsyncPins} />
+                            <Route
+                                exact
+                                path="/explore"
+                                component={AsyncStarList}
+                            />
+                            <Route exact path="/collect" component={Collect} />
+                            <Route exact path="/user" component={Collect} />
+                            <Route path="/:domain" component={AsyncStar} />
+                        </Switch>
+                    </Suspense>
                     {/* toast */}
-                    <StarToast /> 
+                    <StarToast />
                 </div>
-        </Router>
-        )
+            </Router>
+        );
     }
-    
 }
