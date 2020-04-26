a8533013073ed22d8aff9458285cd8c69777d9fe
honestyman <leel50296@gmail.com>
2020-04-26 21:40:26 +0800

    增加备案信息

--- src/containers/WebApp/index.js
+++ src/containers/WebApp/index.js
@@ -12,29 +12,54 @@ const AsyncStarList = lazy(() => import("../StarList"));
 const AsyncStar = lazy(() => import("../Star"));
 
 export default class WebApp extends Component {
-    render() {
-        return (
-            <Router>
-                <div className="webapp">
-                    <Header />
-                    {/* 使用加载指示器为此组件做优雅降级 */}
-                    <Suspense fallback={<div style={{textAlign: "center",padding: "20px 15px"}}>Loading...</div>}>
-                        <Switch>
-                            <Route exact path="/" component={AsyncPins} />
-                            <Route
-                                exact
-                                path="/explore"
-                                component={AsyncStarList}
-                            />
-                            <Route exact path="/collect" component={Collect} />
-                            <Route exact path="/user" component={Collect} />
-                            <Route path="/:domain" component={AsyncStar} />
-                        </Switch>
-                    </Suspense>
-                    {/* toast */}
-                    <StarToast />
-                </div>
-            </Router>
-        );
-    }
+	render() {
+		return (
+			<Router>
+				<div className="webapp">
+					<Header />
+					{/* 使用加载指示器为此组件做优雅降级 */}
+					<Suspense
+						fallback={
+							<div style={{ textAlign: "center", padding: "20px 15px" }}>
+								Loading...
+							</div>
+						}
+					>
+						<Switch>
+							<Route exact path="/" component={AsyncPins} />
+							<Route exact path="/explore" component={AsyncStarList} />
+							<Route exact path="/collect" component={Collect} />
+							<Route exact path="/user" component={Collect} />
+							<Route path="/:domain" component={AsyncStar} />
+						</Switch>
+					</Suspense>
+					<footer>
+                        <p style={{
+                            position: 'fixed',
+                            left: 0,
+                            right: 0,
+                            bottom: 0,
+                            color: '#fff',
+                            margin: 0,
+                            padding: '10px 15px',
+                            background: '#000',
+                            textAlign: 'center',
+                            opacity: '0.7'
+                        }}>
+							<a
+								href="http://www.beian.miit.gov.cn"
+								target="_blank"
+								style={{ color: "#999",marginRight: '10px' }}
+							>
+								鲁ICP备 - 15006514号-7
+							</a>
+							&copy; myhistorybook.cn@2020
+						</p>
+					</footer>
+					{/* toast */}
+					<StarToast />
+				</div>
+			</Router>
+		);
+	}
 }
